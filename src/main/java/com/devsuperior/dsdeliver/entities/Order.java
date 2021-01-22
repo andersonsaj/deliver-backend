32d3f10abcb6032e3151d9c14f7478d251318697
andersonsaj <andersonsb09@hotmail.com>
2021-01-22 11:38:41 -0300

    POST / orders

--- src/main/java/com/devsuperior/dsdeliver/entities/Order.java
+++ src/main/java/com/devsuperior/dsdeliver/entities/Order.java
@@ -37,9 +37,18 @@ public class Order implements Serializable {
 
 	public Order() {
 	}
+	
+	public Order(Long id, String address, Double latitude, Double longitude, Instant moment, OrderStatus status) {
+		super();
+		this.id = id;
+		this.address = address;
+		this.latitude = latitude;
+		this.longitude = longitude;
+		this.moment = moment;
+		this.status = status;
+	}
 
-	public Order(Long id, String address, Double latitude, Double longitude, Instant moment, OrderStatus status,
-			Set<Product> products) {
+	public Order(Long id, String address, Double latitude, Double longitude, Instant moment, OrderStatus status, Set<Product> products) {
 		super();
 		this.id = id;
 		this.address = address;
